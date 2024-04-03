7ebcfb5ff6df5acffd8c43da4469364f60e3edbd
inder782 <inderchandel782@gmail.com>
2024-04-03 20:56:37 +0530

    feat(footer):added and completed responsive footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/Footer";
 import Section_1 from "@/components/Section_1/Section_1";
 import Section_2 from "@/components/Section_2/Section_2";
 import Section_3 from "@/components/Section_3/Section_3";
@@ -5,6 +6,7 @@ import Section_4 from "@/components/Section_4/Section_4";
 import Section_5 from "@/components/Section_5/Section_5";
 import Section_6 from "@/components/Section_6/Section_6";
 import Nav_bottom from "@/components/bottom_nav/Nav_bottom";
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 const page = () => {
@@ -17,6 +19,8 @@ const page = () => {
       <Section_5 />
       <Section_6 />
       <Nav_bottom />
+      <Separator className="mt-10" />
+      <Footer />
     </div>
   );
 };
---
+++ static/socials.ts
@@ -0,0 +1,22 @@
+export const socials = [
+  {
+    key: 1,
+    icon: "footer/fb.svg",
+  },
+  {
+    key: 2,
+    icon: "footer/instagram.svg",
+  },
+  {
+    key: 3,
+    icon: "footer/linkedin.svg",
+  },
+  {
+    key: 4,
+    icon: "footer/Vector.svg",
+  },
+  {
+    key: 5,
+    icon: "footer/youtube.svg",
+  },
+];
