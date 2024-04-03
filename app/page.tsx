1146eafbf22ebc7e8d25963fb16f7320a64b7d3d
inder782 <inderchandel782@gmail.com>
2024-04-03 10:38:34 +0530

    feat(section_5):completed and added responsive section_5

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Section_1 from "@/components/Section_1/Section_1";
 import Section_2 from "@/components/Section_2/Section_2";
 import Section_3 from "@/components/Section_3/Section_3";
 import Section_4 from "@/components/Section_4/Section_4";
+import Section_5 from "@/components/Section_5/Section_5";
 import React from "react";
 
 const page = () => {
@@ -11,6 +12,7 @@ const page = () => {
       <Section_2 />
       <Section_3 />
       <Section_4 />
+      <Section_5 />
     </div>
   );
 };
---
+++ static/tools.ts
@@ -0,0 +1,56 @@
+export const tools = [
+  {
+    key: 1,
+    image: "/Section_5/sec_5_1.svg",
+    heading: "Sapien amet est",
+    desc: "Viverra netus odio nunc ac nibh lorem iaculis. Orci facilisis habitasse cursus tempor et.",
+  },
+  {
+    key: 2,
+    image: "/Section_5/sec_5_2.svg",
+    heading: "Tortor ornare",
+    desc: "Nulla etiam mattis amet suspendisse lacus nulla sed urna ultricies. ",
+  },
+  {
+    key: 3,
+    image: "/Section_5/sec_5_3.svg",
+    heading: "Vel lacus",
+    desc: "Porttitor quis lectus varius quis dolor morbi massa pellentesque.",
+  },
+  {
+    key: 4,
+    image: "/Section_5/sec_5_4.svg",
+    heading: "Hendrerit blandit",
+    desc: "A aliquam pharetra in nec vitae ultrices commodo placerat eget.",
+  },
+  {
+    key: 5,
+    image: "/Section_5/sec_5_5.svg",
+    heading: "Enim risus turpist",
+    desc: "Tristique dui vestibulum senectus ac duis etiam adipiscing sapien.",
+  },
+  {
+    key: 6,
+    image: "/Section_5/sec_5_6.svg",
+    heading: "Aliquam lorem et",
+    desc: "Viverra netus odio nunc ac nibh lorem iaculis. Orci facilisis habitasse cursus tempor et.",
+  },
+  {
+    key: 7,
+    image: "/Section_5/sec_5_7.svg",
+    heading: "Sed aliquam lectus",
+    desc: "Nibh cursus ultricies id neque proin urna vivamus bibendum. Quam sit platea integer sit.",
+  },
+  {
+    key: 8,
+    image: "/Section_5/sec_5_8.svg",
+    heading: "Cursus diam",
+    desc: "Rhoncus ut montes quis eget vulputate. Id hac consectetur ornare neque.",
+  },
+  {
+    key: 9,
+    image: "/Section_5/sec_5_9.svg",
+    heading: "Facilisi vel malesuada",
+    desc: "Feugiat consectetur eu commodo odio venenatis pretium libero. Volutpat ultricies nisl turpis id.",
+  },
+];
